2d71c29da5380f26038c443d0bf9a34938f3c385
Christopher Hong <hintonisbae@gmail.com>
2021-10-01 17:44:22 -0400

    Output multiple rotations in directory

--- src/rotate_pcd/src/main.cpp
+++ src/rotate_pcd/src/main.cpp
@@ -12,6 +12,7 @@ using Eigen::Vector3f;
 using Eigen::AngleAxisf;
 typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
 using namespace std;
+using boost::filesystem::path;
 
 int main(int argc, char** argv)
 {
@@ -19,23 +20,42 @@ int main(int argc, char** argv)
     cerr << "Requires 2 arguments: pcd input and pcd output path" << endl;
     return 1;
   }
-
-  Matrix3f M;
-  M = AngleAxisf(M_PI / 4, Vector3f::UnitZ())
-      * AngleAxisf(M_PI / 4, Vector3f::UnitY()); 
+  path inputFilepath = path(argv[1]);
+  path outputDir = path(argv[2]);
+  string inputFilenameStem = inputFilepath.stem().string();
    
   PointCloud::Ptr inputCloud(new PointCloud);
-  PointCloud::Ptr outputCloud(new PointCloud);
-  pcl::io::loadPCDFile(argv[1], *inputCloud);
+  pcl::io::loadPCDFile(inputFilepath.string(), *inputCloud);
 
-  for (auto point: *inputCloud) {
-    Vector3f p(point.x, point.y, point.z);
-    Vector3f rotated_p = M * p;
-    pcl::PointXYZ newPoint(rotated_p[0], rotated_p[1], rotated_p[2]);
-    outputCloud->push_back(newPoint);
-  } 
+  float startAngle = 0;
+  float endAngle = M_PI;
+  float numIntervals = 20;
+  float intervalAngle = (endAngle - startAngle) / numIntervals;
 
-  pcl::io::savePCDFile(argv[2], *outputCloud);
+  float currAngle = startAngle;
+  int i = 0;
+  while (currAngle < endAngle) {
+    PointCloud::Ptr outputCloud(new PointCloud);
+
+    Matrix3f M;
+    M = AngleAxisf(currAngle, Vector3f::UnitZ());
+    //    * AngleAxisf(M_PI / 4, Vector3f::UnitY()); 
+    for (auto point: *inputCloud) {
+      Vector3f p(point.x, point.y, point.z);
+      Vector3f rotated_p = M * p;
+      pcl::PointXYZ newPoint(rotated_p[0], rotated_p[1], rotated_p[2]);
+      outputCloud->push_back(newPoint);
+    } 
+
+    string outputFilename = inputFilenameStem + "_rot" + to_string(i) + ".pcd";
+    path outputFilepath = outputDir / outputFilename;
+    pcl::io::savePCDFile(outputFilepath.string(), *outputCloud);
+
+    currAngle += intervalAngle;
+    i += 1;
+  }
+
+
 
   return 0;
 }	
