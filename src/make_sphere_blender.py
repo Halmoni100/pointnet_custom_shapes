b0a5f7d53b4b13dc3d7d8160b131854481e6b37e
Christopher Hong <hintonisbae@gmail.com>
2021-10-02 13:32:38 -0400

    Add sphere script

---
+++ src/make_sphere_blender.py
@@ -0,0 +1,17 @@
+import bpy
+import bmesh
+
+scene = bpy.data.scenes['Scene']
+
+mesh = bpy.data.meshes.new('Basic_Sphere')
+
+bm = bmesh.new()
+bmesh.ops.create_uvsphere(bm, u_segments=32, v_segments=16, diameter=1)
+bm.to_mesh(mesh)
+bm.free()
+
+basic_sphere = bpy.data.objects.new('Basic_Sphere', mesh)
+scene.collection.objects.link(basic_sphere)
+
+bpy.ops.export_scene.obj(filepath="/home/mrincredible/temp/test_sphere.obj",
+                         check_existing=False)
